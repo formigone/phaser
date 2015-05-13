1c8955f214d5105c19cf166ac3f09ecfbb4119c9
Rodrigo Silveira <rsilveira@deseretdigital.com>
2015-05-13 05:25:05 -0300

    Some tests for math/Math

--- tests/specs/math/mathSpec.js
+++ tests/specs/math/mathSpec.js
@@ -5,5 +5,21 @@ describe('math Math object', function(){
         var epsi = 0.1;
 
         expect(Phaser.Math.fuzzyEqual(a, b, epsi)).toBe(true);
+
+        a = 0.10;
+        b = 0.20;
+        epsi = 0.01;
+
+        expect(Phaser.Math.fuzzyEqual(a, b, epsi)).toBe(false);
+    });
+
+    it('calculates average of all values passed in', function(){
+        //expect(Phaser.Math.average()).toBe(0);
+        expect(Phaser.Math.average(0)).toBe(0);
+        expect(Phaser.Math.average(1)).toBe(1);
+
+        for (var i = 1; i < 10; i++) {
+
+        }
     });
 });
